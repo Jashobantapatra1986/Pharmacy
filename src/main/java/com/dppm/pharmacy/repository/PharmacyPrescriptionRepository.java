cd45cc557a80f0e2c6f254b2bf4abde0981868ef
Jashobanta <jashobantapatra35@gmail.com>
2026-09-23 13:07:02 +0530

    modified some changes

--- src/main/java/com/dppm/pharmacy/repository/PharmacyPrescriptionRepository.java
+++ src/main/java/com/dppm/pharmacy/repository/PharmacyPrescriptionRepository.java
@@ -3,7 +3,10 @@ package com.dppm.pharmacy.repository;
 import com.dppm.pharmacy.entity.PharmacyPrescription;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface PharmacyPrescriptionRepository extends ReactiveCrudRepository<PharmacyPrescription, Long> {
     Flux<PharmacyPrescription> findByPharmacyIdOrderByCreatedAtDesc(Long pharmacyId);
+
+    Mono<PharmacyPrescription> findByPharmacyIdAndPrescriptionId(Long pharmacyId, Long prescriptionId);
 }
